cab02b08fb2517d68afb0ba316511f086b90bff0
Rishik Reddy Endurthy <S567536@nwmissouri.edu>
2024-03-08 23:24:17 -0600

    added VC

---
+++ QuizyQuiz/QuizyQuiz/App/CreateQuizVC.swift
@@ -0,0 +1,29 @@
+//
+//  CreateQuizVC.swift
+//  QuizyQuiz
+//
+//  Created by Rishik Reddy Endurthy on 3/8/24.
+//
+
+import UIKit
+
+class CreateQuizVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
