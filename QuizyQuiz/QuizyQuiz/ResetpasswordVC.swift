a96e8ca0dce219a1126d87a244745c1f22a9220b
Hema Pradeepthi <s566595@nwmissouri.edu>
2024-03-27 21:17:36 -0600

    Hema Pradeepthi: Gradient view added for reset password VC

--- QuizyQuiz/QuizyQuiz/ResetpasswordVC.swift
+++ QuizyQuiz/QuizyQuiz/ResetpasswordVC.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import FirebaseAuth
+import AnimatedGradientView
 
 class ResetpasswordVC: UIViewController {
 
@@ -48,7 +49,18 @@ class ResetpasswordVC: UIViewController {
                 throw error
             }
         }
-    
+    private func applyGradientBackground(){
+            let gradientType: CAGradientLayerType = .axial
+            let direction: AnimatedGradientViewDirection = .down
+            let animatedGradient = AnimatedGradientView(frame: self.view.bounds)
+            animatedGradient.animationValues = [
+                (colors: ["ffafbd","cc2b5e"],direction,gradientType),
+                (colors: ["42275a","bdc3c7"],direction,gradientType),
+                (colors: ["de6262","dd5e89"],direction,gradientType),
+                (colors: ["06beb6","2193b0"],direction,gradientType),
+            ]
+            self.view.insertSubview(animatedGradient, at:0)
+        }
     /*
     // MARK: - Navigation
 
