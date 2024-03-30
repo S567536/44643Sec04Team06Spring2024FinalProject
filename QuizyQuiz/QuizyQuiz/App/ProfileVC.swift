bb8eb4268f679ae9312a2f2e2dc055c2577f8f24
Rishik Reddy Endurthy <S567536@nwmissouri.edu>
2024-03-29 23:12:11 -0500

    Added Logout Segue

--- QuizyQuiz/QuizyQuiz/App/ProfileVC.swift
+++ QuizyQuiz/QuizyQuiz/App/ProfileVC.swift
@@ -6,24 +6,54 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class ProfileVC: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    @IBAction func Logout(_ sender: Any) {
+        
+        ShowAlert()
+        
+        
+        
     }
-    */
-
+    
+    func ShowAlert(){
+        let alert = UIAlertController(title: "Logout", message: "Would you like to logout", preferredStyle: .alert)
+        let action1 = UIAlertAction(title:"cancel",style: .cancel)
+        let action2 = UIAlertAction(title: "Confirm", style: .default){ _ in
+            do{
+                try  AuthenticationManager.shared.signOut()
+                self.performSegue(withIdentifier: "productsToLogin", sender: nil)
+            }catch{
+                print("error")
+            }}
+        
+        alert.addAction(action1)
+        alert.addAction(action2)
+        
+        present(alert,animated: true,completion: nil)
+        
+        
+    }
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+    
 }
