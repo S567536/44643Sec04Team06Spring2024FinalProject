cce66c28ae3802b06f9b348e6b4d46ac6f2e3b2d
Nelluri047 <nellurimanikantabtech@gmail.com>
2024-04-12 22:51:58 -0500

    Manikanta Nelluri - Profile TABLE VIEW controller AND table view cell is created.

---
+++ QuizyQuiz/QuizyQuiz/ProfileTVCell.swift
@@ -0,0 +1,23 @@
+//
+//  ProfileTVCell.swift
+//  QuizyQuiz
+//
+//  Created by manikanta on 4/12/24.
+//
+
+import UIKit
+
+class ProfileTVCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
