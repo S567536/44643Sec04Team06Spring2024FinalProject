d7b198e35892018596bdc5b610a3ff1920ad1330
Rishik Reddy Endurthy <S567536@nwmissouri.edu>
2024-04-12 22:38:44 -0500

    added Collection view with header and cell and their outlets

---
+++ QuizyQuiz/QuizyQuiz/CollectionContent/HeaderView.swift
@@ -0,0 +1,15 @@
+//
+//  HeaderView.swift
+//  QuizyQuiz
+//
+//  Created by Rishik Reddy Endurthy on 4/12/24.
+//
+
+import UIKit
+
+class HeaderView: UICollectionReusableView {
+    
+    
+    @IBOutlet weak var headetrLBL: UILabel!
+    
+}
---
+++ QuizyQuiz/QuizyQuiz/CollectionContent/Quiz CEll.swift
@@ -0,0 +1,17 @@
+//
+//  Quiz CEll.swift
+//  QuizyQuiz
+//
+//  Created by Rishik Reddy Endurthy on 4/12/24.
+//
+
+import UIKit
+
+class QuizCell: UICollectionViewCell {
+    
+    @IBOutlet weak var image: UIImageView!
+    
+    
+    @IBOutlet weak var imgLBL: UILabel!
+    
+}
