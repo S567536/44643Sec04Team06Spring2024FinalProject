3f7c9301cf5e9250ed02d0b2f5b3add3e9609c5d
Rishik Reddy Endurthy <S567536@nwmissouri.edu>
2024-04-17 21:35:51 -0500

    added

--- QuizyQuiz/QuizyQuiz/CollectionContent/SeeQUiz.swift
+++ QuizyQuiz/QuizyQuiz/CollectionContent/SeeQUiz.swift
@@ -9,42 +9,50 @@ import UIKit
 
 private let reuseIdentifier = "Cell"
 
-class SeeQUiz: UICollectionViewController,UITableViewDelegate, UITableViewDataSource  {
-
-        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-            return models.count
-        }
-        
-        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-            let cell = table.dequeueReusableCell(withIdentifier: seeAllTableViewCell.identifier, for: indexPath)as!seeAllTableViewCell
-            return cell
-        }
-        
-        func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-            return 250.0
-        }
+class SeeQUiz: UICollectionViewController{
+    //    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    //        return1
+    //    }
+    //    
+    //    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    //        1
+    //    }
     
-    @IBOutlet var table: UITableView!
-        var models = [Model]()
-
-        override func viewDidLoad() {
-            super.viewDidLoad()
-            table.register(seeAllTableViewCell.nib(), forCellReuseIdentifier: seeAllTableViewCell.identifier)
-            table.delegate = self
-            table.dataSource = self
-            
-        }
-        
-
-    }
-    struct Model {
-        let tect: String
-        let imageName: String
-        
-        init(tect: String, imageName: String) {
-            self.tect = tect
-            self.imageName = imageName
-        
-        }
-    }
-
+    //
+    //        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    //            return models.count
+    //        }
+    //        
+    //        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    //            let cell = table.dequeueReusableCell(withIdentifier: seeAllTableViewCell.identifier, for: indexPath)as!seeAllTableViewCell
+    //            return cell
+    //        }
+    //        
+    //        func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+    //            return 250.0
+    //        }
+    //    
+    //    @IBOutlet var table: UITableView!
+    //        var models = [Model]()
+    //
+    //        override func viewDidLoad() {
+    //            super.viewDidLoad()
+    //            table.register(seeAllTableViewCell.nib(), forCellReuseIdentifier: seeAllTableViewCell.identifier)
+    //            table.delegate = self
+    //            table.dataSource = self
+    //            
+    //        }
+    //        
+    //
+    //    }
+    //    struct Model {
+    //        let tect: String
+    //        let imageName: String
+    //        
+    //        init(tect: String, imageName: String) {
+    //            self.tect = tect
+    //            self.imageName = imageName
+    //        
+    //        }
+    //    }
+}
--- QuizyQuiz/QuizyQuiz/Home/QuizTopicsVC.swift
+++ QuizyQuiz/QuizyQuiz/Home/QuizTopicsVC.swift
@@ -7,18 +7,63 @@
 
 import UIKit
 
-class QuizTopicsVC: UIViewController {
+class QuizTopicsVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
     
     var topics : [SubjectModel] = []
     
     
-
+    @IBOutlet weak var topicsTV: UITableView!
+    
+    var selectedTopics: SubjectModel?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.topicsTV.delegate = self
+        self.topicsTV.dataSource = self
+        self.topicsTV.clipsToBounds = true
+        
+        
+        Constants.getSubjects{ res in
+            self.topics = res
+            
+            self.topicsTV.reloadData()
+    
+        }
 
         // Do any additional setup after loading the view.
     }
     
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return topics.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .value1, reuseIdentifier: "Cell")
+        
+        let topics = topics[indexPath.row]
+        cell.textLabel?.text = topics.name ?? ""
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        selectedTopics = topics[indexPath.row]
+        self.performSegue(withIdentifier: "quiz", sender: self)
+    }
+    
+    
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        if segue.identifier == "quiz"{
+//            
+//            let vc = segue.destination as! QuizVC
+//            //vc.topics = selectedTopics?.name ?? "Linux"
+//        }
+//    }
 
     /*
     // MARK: - Navigation
