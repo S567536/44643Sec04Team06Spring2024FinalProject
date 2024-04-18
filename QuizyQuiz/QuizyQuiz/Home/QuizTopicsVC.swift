d44d303cf17be7990d37aebf878fd80fd7e5491b
Rishik Reddy Endurthy <S567536@nwmissouri.edu>
2024-04-17 20:37:00 -0500

    added quizTopics Screen

---
+++ QuizyQuiz/QuizyQuiz/Home/QuizTopicsVC.swift
@@ -0,0 +1,33 @@
+//
+//  QuizTopicsVC.swift
+//  QuizyQuiz
+//
+//  Created by Rishik Reddy Endurthy on 4/17/24.
+//
+
+import UIKit
+
+class QuizTopicsVC: UIViewController {
+    
+    var topics : [SubjectModel] = []
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
